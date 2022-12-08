f11ab5120d39897bc56d32e79319a4c96405769d
peterpozo <peteranderson.pozo@gmail.com>
2022-12-08 21:57:50 +0100

    Card component

---
+++ src/components/CardsComponents.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row row-cols-3 g-4">
+    <div class="col"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardsComponents",
+};
+</script>
+
+<style lang="scss" scoped></style>
